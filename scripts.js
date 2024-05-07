a182a929f71a4130d1267e2482b62d717f572719
PhillipHa5 <phillip.ha5@education.nsw.gov.au>
2024-05-07 14:30:22 +1000

    V10
    attempted to improve profile viewer

--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
 let openmenu = document.getElementById("menu");
-        
+
+const Menu = document.querySelector('menu');
+const Profileicon = document.querySelector('profileicon');
+
 function openclosemenu() {
     openmenu.classList.toggle("openedmenu");
+}
+if (Profileicon == null) {
+    console.log('HAIIIII')
+}
+
+let x = 0;
+
+function helpmepls() {
+    if (x == 0) {
+        x = x + 1
+        console.log(x)
+    } else {
+        x = x - 1
+        console.log(x)
+    }
+}
+
+if (x == 0) {
+    openmenu.classList.add("openedmenu")
+}
+
+if (x == 1) {
+    openmenu.classList.remove("openedmenu")
 }
