5f706a65ef74353079bd88fb2e098b3d5e02aece
PhillipHa5 <phillip.ha5@education.nsw.gov.au>
2024-05-15 18:50:07 +1000

    V16
    Form Testing

--- scripts.js
+++ scripts.js
@@ -4,35 +4,27 @@ let bookingformcheck = document.getElementById('bookingform')
 
 document.onclick = ($e) => {
     let x = [$e.srcElement];
-    console.log(x);
-    console.log(x[0])
     let pee = x[0].id;
-    console.log(pee);
     if (pee == 'profileicon') {
-        console.log("MenuOpened!");
         profilemenu.classList.toggle('openedmenu');
     }
     if (pee == 'logintext') {
-        console.log('Clicked On Menu!')
     }
     if (pee == 'menu') {
-        console.log('Clicked On Menu!')
     }
     if (pee == '') {
-        console.log('Menu Closed!')
         profilemenu.classList.remove('openedmenu')
     } else {
         if (pee !== 'profileicon' && pee !== 'menu' && pee !== "logintext") {
-            console.log('Nothing Selected, Menu Closed!');
             profilemenu.classList.remove('openedmenu');
-            console.log(pee)
         }
     }
 }
-let namecheckjs = document.getElementById("namecheck");
-
+let namecheckjs = document.getElementById('namecheck')
 function bookingcheck() {
-    var formdata = new FormData(bookingformcheck)
-    console.log(namecheckjs)
-    console.log(formdata)
+    namecheckjs.classList.remove('empty')
+    if (document.querySelector('#namecheck').value == '') {
+        console.log('Empty')
+        namecheckjs.classList.add('empty')
+    }
 }
